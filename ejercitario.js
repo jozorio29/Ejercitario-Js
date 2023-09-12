52523f5f25190115cca44f52c61db8e82cc75da8
jozorio29 <130694329+jozorio29@users.noreply.github.com>
2023-09-12 17:26:49 -0400

    Add files via upload

---
+++ ejercitario.js
@@ -0,0 +1,11 @@
+let valor1 = parseInt(window.prompt("Ingrese un número"));
+let valor2 = parseInt(window.prompt("Ingrese otro número"));
+
+//Determinar el mayor 
+
+if (valor1 > valor2) {
+    alert("El mayor es el valor: " + valor1);
+} else {
+    alert("El mayor es el valor: " + valor2);
+}
+
